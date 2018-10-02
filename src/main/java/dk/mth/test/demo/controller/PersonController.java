7b500a9535f38120a027a061e77fb4b2ccc39131
MortenGTherkildsen <morten@therkildsen.com>
2018-10-02 11:39:13 +0200

    alt igen

--- src/main/java/dk/mth/test/demo/controller/PersonController.java
+++ src/main/java/dk/mth/test/demo/controller/PersonController.java
@@ -1,8 +1,13 @@
 package dk.mth.test.demo.controller;
 
+import dk.mth.test.demo.model.Customer;
 import dk.mth.test.demo.model.Person;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,9 +19,21 @@ public class PersonController {
         hl = new ArrayList<>();
     }
 
+    @GetMapping(path = "/")
     public String index(Model model){
         model.addAttribute("personData", hl);
         return "index";
+    }
+
+    @GetMapping("/createcustomer")
+    public String createcustomer(@ModelAttribute Model model) {
+        return "createcustomer";
+    }
+
+    @PostMapping("/createcustomer")
+    public String getcreatecustomer(@ModelAttribute Customer customer) {
+        hl.add(customer);
+        return "redirect:/index";
     }
 
 }
