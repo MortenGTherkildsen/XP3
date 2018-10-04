895c79d2dd2308f6ae6a4efe7f6b59d5a73a9c4b
MortenGTherkildsen <morten@therkildsen.com>
2018-10-04 13:39:17 +0200

    Controller omdøbt igen til WebController

--- src/main/java/dk/mth/test/demo/controller/WebController.java
+++ src/main/java/dk/mth/test/demo/controller/WebController.java
@@ -2,22 +2,19 @@ package dk.mth.test.demo.controller;
 
 import dk.mth.test.demo.model.Customer;
 import dk.mth.test.demo.model.Person;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-@Controller
-public class Controller {
+@WebController
+public class WebController {
 
     private List<Person> hl;
 
 
-    public Controller(){
+    public WebController(){
         hl = new ArrayList<>();
     }
 
