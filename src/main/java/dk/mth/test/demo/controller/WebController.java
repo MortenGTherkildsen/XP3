c888d991fb1226a98bb1f8d3165a52094f9fcccd
Killfill0o <phil3229@kea.stud.dk>
2018-10-06 17:28:41 +0200

    Login System tilføjet, man kan ikke tilgå reservationer.html uden at være logget på, hvilket betyder at man skal oprette en customer før man kan logge på og se reservationer.
    Philip

--- src/main/java/dk/mth/test/demo/controller/WebController.java
+++ src/main/java/dk/mth/test/demo/controller/WebController.java
@@ -13,6 +13,8 @@ import java.util.List;
 public class WebController {
 
     private List<Person> hl;
+    private Person user = new Person("","");
+    private boolean loginstatus = false;
 
 
     public WebController(){
@@ -36,13 +38,18 @@ public class WebController {
 
         System.out.println(email + " " + password);
         hl.add(new Customer(name,birthday, email,password,"+"));
+        user.setEmail(email); user.setPassword(password);
         return index(model);
     }
 
-    @GetMapping(path = "/aktiviteter")
-    public String aktiviteter() {
-
-        return "/aktiviteter";
+    @GetMapping(path = "/reservationer")
+    public String reservationer() {
+        if(loginstatus) {
+            System.out.println(" DEBUG : " + user.getEmail() + " " + user.getPassword());
+            return "/reservationer";
+        }
+        System.out.println(" DEBUG2 : " + user.getEmail() + " " + user.getPassword());
+        return "/login";
     }
 
     @GetMapping(path = "/login")
@@ -51,15 +58,39 @@ public class WebController {
         return "/login";
     }
 
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String login(@RequestParam String email, @RequestParam String password){
+        if(email.equals(user.getEmail())&&!email.equals("")){
+            System.out.println("Email match successfully : "+email);
+            if(password.equals(user.getPassword())&&!password.equals("")){
+                System.out.println("password match successfully : "+password);
+                loginstatus = true;
+                System.out.println("User successfully logged in : "+email+" | "+password);
+                return "/reservationer";
+            }
+            else
+            {
+                System.out.println("password DO NOT match");
+                return "/login";
+            }
+        }
+        else
+        {
+            System.out.println("email DO NOT match");
+            return "/login";
+        }
+    }
+
     @GetMapping(path = "/opret")
     public String opret() {
 
         return "/opret";
     }
 
-    @GetMapping(path = "/reservationer")
-    public String reservationer() {
+    @GetMapping(path = "/aktiviteter")
+    public String aktiviteter() {
 
-        return "/reservationer";
+        return "/aktiviteter";
     }
+
 }
